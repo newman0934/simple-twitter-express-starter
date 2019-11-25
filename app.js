1650c0c9a8c2a039d0b7fad29fc422a6533a6d84
Eason Lin <uu9924079@gmail.com>
2019-11-25 20:26:17 +0800

    init

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const helpers = require('./_helpers');
+const helpers = require('./_helpers')
 
 const app = express()
 const port = 3000
@@ -8,6 +8,6 @@ const port = 3000
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!!!`))
 
 module.exports = app
