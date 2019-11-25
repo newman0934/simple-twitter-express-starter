aae8440be6c270a874d9a01a355c314ff4daa338
Eason Lin <uu9924079@gmail.com>
2019-11-25 20:43:05 +0800

    pull test

--- app.js
+++ app.js
@@ -7,12 +7,7 @@ const port = 3000
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
 
-<<<<<<< HEAD
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!!!`))
-=======
 app.get('/', (req, res) => res.send('Hello World!!!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
->>>>>>> c03a2589d2b821d6bc9e96aa4c7ca335ed6a7a50
 
 module.exports = app
