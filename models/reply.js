2955d63a75f9c749ddcb20400183fac183d4a2de
Eason Lin <uu9924079@gmail.com>
2019-11-26 13:46:15 +0800

    add table props

--- models/reply.js
+++ models/reply.js
@@ -1,6 +1,14 @@
 'use strict'
 module.exports = (sequelize, DataTypes) => {
-  const Reply = sequelize.define('Reply', {}, {})
+  const Reply = sequelize.define(
+    'Reply',
+    {
+      TweetId: DataTypes.INTEGER,
+      UserId: DataTypes.INTEGER,
+      comment: DataTypes.TEXT
+    },
+    {}
+  )
   Reply.associate = function(models) {
     Reply.belongsTo(models.User)
     Reply.belongsTo(models.Tweet)
--- models/user.js
+++ models/user.js
@@ -1,6 +1,17 @@
 'use strict'
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {}, {})
+  const User = sequelize.define(
+    'User',
+    {
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      name: DataTypes.STRING,
+      avatar: DataTypes.STRING,
+      introduction: DataTypes.TEXT,
+      role: DataTypes.STRING
+    },
+    {}
+  )
   User.associate = function(models) {
     User.hasMany(models.Tweet)
     User.hasMany(models.Like)
