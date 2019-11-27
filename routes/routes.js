4d7b8219ca6382a28dff0ec1d02aea0ae6c4ccf7
java5575 <newman0934@gmail.com>
2019-11-27 22:41:19 +0800

    在每個路由加上登入驗證

--- routes/routes.js
+++ routes/routes.js
@@ -20,30 +20,32 @@ const authenticatedAdmin = (req, res, next) => {
   res.redirect("/signin")
 }
 
-router.get('/', (req, res) => res.redirect('/tweets'))
-router.get('/tweets', (req, res) => res.render('tweets'))
-router.post('/tweets', (req, res) => res.send('post /tweets'))
-router.get('/tweets/:tweet_id/replies', (req, res) => res.render('replies'))
-router.post('/tweets/:tweet_id/replies', (req, res) =>
+router.get('/', authenticated, (req, res) => res.redirect('/tweets'))
+router.get('/tweets', authenticated, (req, res) => res.render('tweets'))
+router.post('/tweets', authenticated, (req, res) => res.send('post /tweets'))
+router.get('/tweets/:tweet_id/replies', authenticated, (req, res) => res.render('replies'))
+router.post('/tweets/:tweet_id/replies', authenticated, (req, res) =>
   res.send('post /tweets/:tweet_id/replies')
 )
-router.get('/users/:id/tweets', (req, res) => res.render('user/user'))
-router.get('/users/:id/followings', (req, res) => res.render('user/followings'))
-router.get('/users/:id/followers', (req, res) => res.render('user/followers'))
-router.post('/followships', (req, res) => res.send('post /followships'))
-router.delete('/followships/:followingId', (req, res) =>
+router.get('/users/:id/tweets', authenticated, (req, res) => res.render('user/user'))
+router.get('/users/:id/followings', authenticated, (req, res) => res.render('user/followings'))
+router.get('/users/:id/followers',authenticated,  (req, res) => res.render('user/followers'))
+router.post('/followships',authenticated, (req, res) => res.send('post /followships'))
+router.delete('/followships/:followingId', authenticated, (req, res) =>
   res.send('delete /followships/:followingId')
 )
-router.get('/users/:id/likes', (req, res) => res.render('user/like'))
-router.post('/tweets/:id/like', (req, res) => res.send('post /tweets/:id/like'))
-router.post('/tweets', (req, res) => res.send('post /tweets/:id/unlike'))
-router.get('/users/:id/edit', (req, res) => res.render('user/edit'))
-router.post('/users/:id/edit', (req, res) => res.send('post /users/:id/edit'))
-router.get('/admin/tweets', (req, res) => res.render('admin/tweets'))
-router.delete('/admin/tweets/:id', (req, res) =>
+router.get('/users/:id/likes', authenticated, (req, res) => res.render('user/like'))
+router.post('/tweets/:id/like', authenticated, (req, res) => res.send('post /tweets/:id/like'))
+router.post('/tweets', authenticated, (req, res) => res.send('post /tweets/:id/unlike'))
+router.get('/users/:id/edit', authenticated, (req, res) => res.render('user/edit'))
+router.post('/users/:id/edit', authenticated, (req, res) => res.send('post /users/:id/edit'))
+router.get('/admin/tweets', authenticatedAdmin,  (req, res) => res.render('admin/tweets'))
+router.delete('/admin/tweets/:id', authenticatedAdmin, (req, res) =>
   res.send('delete /admin/tweets/:id')
 )
-router.get('/admin/users', (req, res) => res.render('admin/users'))
+
+router.get("/admin", authenticatedAdmin, (req, res) => res.redirect("/admin/tweets"))
+router.get('/admin/users', authenticatedAdmin, (req, res) => res.render('admin/users'))
 router.get("/signup", userController.signUpPage)
 router.post("/signup", userController.signUp)
 router.get("/signin", userController.signInPage)
@@ -55,5 +57,5 @@ router.post(
   }),
   userController.signIn
 );
-
+router.get("/logout",userController.logout)
 module.exports = router
