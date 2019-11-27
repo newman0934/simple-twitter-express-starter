11a091768fd2e00f5549bc471de628ea7ad12935
Eason Lin <uu9924079@gmail.com>
2019-11-27 16:51:46 +0800

    send reply.length into user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,14 @@
 const db = require('../models')
 const User = db.User
 const Tweet = db.Tweet
+const Reply = db.Reply
 
 const userController = {
   getUserTweets: (req, res) => {
-    return User.findByPk(req.params.id, { include: Tweet }).then(user => {
+    return User.findByPk(req.params.id, {
+      include: { model: Tweet, include: [Reply] }
+    }).then(user => {
+      console.log(user)
       const tweets = user.Tweets
       res.render('user/user', { user, tweets })
     })
