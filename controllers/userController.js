74de45b7bb36b724d1a671b04b306bfa49ee92b7
Eason Lin <uu9924079@gmail.com>
2019-11-28 13:58:50 +0800

    add addFollowing and removeFollowing

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,65 +1,90 @@
-const bcrypt = require("bcrypt-nodejs")
-const db = require("../models")
+const bcrypt = require('bcrypt-nodejs')
+const db = require('../models')
 const User = db.User
 const Tweet = db.Tweet
 const Reply = db.Reply
+const Followship = db.Followship
 
 let userController = {
-    signInPage: (req, res) => {
-        return res.render("signin")
-    },
-    signIn: (req, res) => {
-        req.flash("success_messages", "成功登入")
-        return res.redirect("/tweets")
-    },
-    signUpPage: (req, res) => {
-        return res.render("signup")
-    },
-    signUp: (req, res) => {
-        if (req.body.passwordCheck != req.body.password) {
-            req.flash("error_messages", "兩次密碼輸入不同")
-            return res.redirect("/signup")
-        } else {
-            User.findOne({
-                where: {
-                    email: req.body.email
-                }
-            }).then(user => {
-                if (user) {
-                    req.flash("error_messages", "信箱重複")
-                    return res.redirect("/signup")
-                } else {
-                    User.create({
-                        name: req.body.name,
-                        email: req.body.email,
-                        password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
-                    }).then(user => {
-                        return res.redirect("/signin")
-                    })
-                }
-            })
+  signInPage: (req, res) => {
+    return res.render('signin')
+  },
+  signIn: (req, res) => {
+    req.flash('success_messages', '成功登入')
+    return res.redirect('/tweets')
+  },
+  signUpPage: (req, res) => {
+    return res.render('signup')
+  },
+  signUp: (req, res) => {
+    if (req.body.passwordCheck != req.body.password) {
+      req.flash('error_messages', '兩次密碼輸入不同')
+      return res.redirect('/signup')
+    } else {
+      User.findOne({
+        where: {
+          email: req.body.email
         }
-    },
-    logout: (req, res) => {
-        req.flash("success_messages", "登出成功")
-        req.logout()
-        res.redirect("/signin")
-    },
-    getUserTweets: (req, res) => {
-        return User.findByPk(req.params.id, {
-          include: { model: Tweet, include: [Reply] }
-        }).then(user => {
-          const tweets = user.Tweets
-          res.render('user/user', { user, tweets })
-        })
-      },
-    
-      getUserEdit: (req, res) => {
-        return User.findByPk(req.params.id).then(user => {
-          res.render('user/edit', { user })
-        })
+      }).then(user => {
+        if (user) {
+          req.flash('error_messages', '信箱重複')
+          return res.redirect('/signup')
+        } else {
+          User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: bcrypt.hashSync(
+              req.body.password,
+              bcrypt.genSaltSync(10),
+              null
+            ),
+            avatar: 'https://fakeimg.pl/300x300/'
+          }).then(user => {
+            return res.redirect('/signin')
+          })
+        }
+      })
+    }
+  },
+  logout: (req, res) => {
+    req.flash('success_messages', '登出成功')
+    req.logout()
+    res.redirect('/signin')
+  },
+  getUserTweets: (req, res) => {
+    return User.findByPk(req.params.id, {
+      include: { model: Tweet, include: [Reply] }
+    }).then(user => {
+      const tweets = user.Tweets
+      res.render('user/user', { user, tweets })
+    })
+  },
+
+  getUserEdit: (req, res) => {
+    return User.findByPk(req.params.id).then(user => {
+      res.render('user/edit', { user })
+    })
+  },
+
+  addFollowing: (req, res) => {
+    return Followship.create({
+      followerId: req.user.id,
+      followingId: req.params.id
+    }).then(followship => {
+      return res.redirect('back')
+    })
+  },
+
+  removeFollowing: (req, res) => {
+    return Followship.findOne({
+      where: {
+        followerId: req.user.id,
+        followingId: req.params.userId
       }
+    }).then(followship => {
+      return res.redirect('back')
+    })
+  }
 }
 
 module.exports = userController
-
--- routes/routes.js
+++ routes/routes.js
@@ -1,63 +1,89 @@
 const express = require('express')
 const router = express.Router()
-const userController = require("../controllers/userController")
-const passport = require("../config/passport")
+const userController = require('../controllers/userController')
+const passport = require('../config/passport')
 const tweetController = require('./../controllers/tweetController')
 const adminController = require('./../controllers/adminController')
 const replyController = require('./../controllers/replyController')
 
-
 const authenticated = (req, res, next) => {
-  if(req.isAuthenticated()){
+  if (req.isAuthenticated()) {
     return next()
   }
-  res.redirect("/signin")
+  res.redirect('/signin')
 }
 
 const authenticatedAdmin = (req, res, next) => {
-  if(req.isAuthenticated()){
-    if(req.user.role){
+  if (req.isAuthenticated()) {
+    if (req.user.role) {
       return next()
     }
-    return res.redirect("/")
+    return res.redirect('/')
   }
-  res.redirect("/signin")
+  res.redirect('/signin')
 }
 
 router.get('/', authenticated, (req, res) => res.redirect('/tweets'))
 router.get('/tweets', authenticated, tweetController.getTweets)
 router.post('/tweets', authenticated, tweetController.postTweets)
-router.get('/tweets/:tweet_id/replies', authenticated, replyController.getTweetReply)
-router.post('/tweets/:tweet_id/replies', authenticated, replyController.postTweetReply)
+router.get(
+  '/tweets/:tweet_id/replies',
+  authenticated,
+  replyController.getTweetReply
+)
+router.post(
+  '/tweets/:tweet_id/replies',
+  authenticated,
+  replyController.postTweetReply
+)
 
 router.get('/users/:id/tweets', authenticated, userController.getUserTweets)
-router.get('/users/:id/followings', authenticated, (req, res) => res.render('user/followings'))
-router.get('/users/:id/followers',authenticated,  (req, res) => res.render('user/followers'))
-router.post('/followships',authenticated, (req, res) => res.send('post /followships'))
-router.delete('/followships/:followingId', authenticated, (req, res) =>
-  res.send('delete /followships/:followingId')
+router.get('/users/:id/followings', authenticated, (req, res) =>
+  res.render('user/followings')
 )
-router.get('/users/:id/likes', authenticated, (req, res) => res.render('user/like'))
-router.post('/tweets/:id/like', authenticated, (req, res) => res.send('post /tweets/:id/like'))
-router.post('/tweets', authenticated, (req, res) => res.send('post /tweets/:id/unlike'))
+router.get('/users/:id/followers', authenticated, (req, res) =>
+  res.render('user/followers')
+)
+router.post('/followships', authenticated, userController.addFollowing)
+router.delete(
+  '/followships/:followingId',
+  authenticated,
+  userController.removeFollowing
+)
+router.get('/users/:id/likes', authenticated, (req, res) =>
+  res.render('user/like')
+)
+router.post('/tweets/:id/like', authenticated, (req, res) =>
+  res.send('post /tweets/:id/like')
+)
+router.post('/tweets', authenticated, (req, res) =>
+  res.send('post /tweets/:id/unlike')
+)
 router.get('/users/:id/edit', authenticated, userController.getUserEdit)
-router.post('/users/:id/edit', authenticated, (req, res) => res.send('post /users/:id/edit'))
-router.get('/admin/tweets', authenticatedAdmin,  adminController.getTweets)
-router.delete('/admin/tweets/:id', authenticatedAdmin, adminController.deleteTweet)
+router.post('/users/:id/edit', authenticated, (req, res) =>
+  res.send('post /users/:id/edit')
+)
+router.get('/admin/tweets', authenticatedAdmin, adminController.getTweets)
+router.delete(
+  '/admin/tweets/:id',
+  authenticatedAdmin,
+  adminController.deleteTweet
+)
 
-
-router.get("/admin", authenticatedAdmin, (req, res) => res.redirect("/admin/tweets"))
+router.get('/admin', authenticatedAdmin, (req, res) =>
+  res.redirect('/admin/tweets')
+)
 router.get('/admin/users', authenticatedAdmin, adminController.getUsers)
-router.get("/signup", userController.signUpPage)
-router.post("/signup", userController.signUp)
-router.get("/signin", userController.signInPage)
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
+router.get('/signin', userController.signInPage)
 router.post(
-  "/signin",
-  passport.authenticate("local", {
-      failureRedirect: "/signin",
-      failureFlash: true
+  '/signin',
+  passport.authenticate('local', {
+    failureRedirect: '/signin',
+    failureFlash: true
   }),
   userController.signIn
-);
-router.get("/logout",userController.logout)
+)
+router.get('/logout', userController.logout)
 module.exports = router
