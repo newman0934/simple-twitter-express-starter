47357b07c6dc1a84606d9f8377e8f887e59c3e64
Eason Lin <uu9924079@gmail.com>
2019-11-28 14:54:15 +0800

    include bodyParser

--- app.js
+++ app.js
@@ -1,37 +1,40 @@
 const express = require('express')
 const helpers = require('./_helpers')
 const handlebars = require('express-handlebars')
-const passport = require("./config/passport")
-const flash = require("connect-flash")
-const session = require("express-session")
+const passport = require('./config/passport')
+const flash = require('connect-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
 
 const app = express()
 const port = 3000
-const bodyParser = require("body-parser")
+const bodyParser = require('body-parser')
 
 app.set('view engine', 'handlebars')
 app.engine(
   'handlebars',
   handlebars({
     defaultLayout: 'main',
-    helpers: require("./config/handlebars-helpers")
+    helpers: require('./config/handlebars-helpers')
   })
 )
 
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(session({secret:"secret", resave:"false",saveUninitialized:"false"}))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(
+  session({ secret: 'secret', resave: 'false', saveUninitialized: 'false' })
+)
 app.use(flash())
 
 app.use(passport.initialize())
 app.use(passport.session())
+app.use(methodOverride('_method'))
 
 app.use((req, res, next) => {
-  res.locals.success_messages = req.flash("success_messages")
-  res.locals.error_messages = req.flash("error_messages")
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
   res.locals.user = req.user
   next()
 })
-
 
 // use helpers.getUser(req) to replace req.user
 // use helpers.ensureAuthenticated(req) to replace req.isAuthenticated()
