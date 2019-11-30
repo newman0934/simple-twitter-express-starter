242e6e3955d79d660360a61f6d8c070f71b1d0ed
Eason Lin <uu9924079@gmail.com>
2019-11-30 20:56:36 +0800

    finished /tweets/:id/replies page

--- controllers/replyController.js
+++ controllers/replyController.js
@@ -2,15 +2,18 @@ const db = require('../models')
 const Tweet = db.Tweet
 const User = db.User
 const Reply = db.Reply
+const Like = db.Like
 
 const replyController = {
   getTweetReply: (req, res) => {
     return Tweet.findByPk(req.params.tweet_id, {
       include: [
+        Like,
         { model: Reply, include: [User] },
         {
           model: User,
           include: [
+            Like,
             Tweet,
             { model: User, as: 'Followers' },
             { model: User, as: 'Followings' }
@@ -24,13 +27,16 @@ const replyController = {
       const isFollowed = tweetUser.Followers.map(d => d.id).includes(
         currentUser.id
       )
-      console.log(isFollowed)
+      const likedByUser = tweet.Likes.map(like => like.UserId).includes(
+        currentUser.id
+      )
       return res.render('replies', {
         tweet,
         replies,
         tweetUser,
         currentUser,
-        isFollowed
+        isFollowed,
+        likedByUser
       })
     })
   },
