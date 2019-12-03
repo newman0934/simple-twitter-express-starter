4c22e7609963c2de9dcb0f0eaf0b0e2167bb9fd2
java5575 <newman0934@gmail.com>
2019-12-03 22:02:48 +0800

    edit handlebars-helpers

--- config/handlebars-helpers.js
+++ config/handlebars-helpers.js
@@ -1,18 +1,6 @@
 const moment = require('moment')
 module.exports = {
-<<<<<<< HEAD
-    ifCond: function(a, b, options){
-        if( a === b){
-            return options.fn(this)
-        }else{
-            return options.inverse(this)
-        }
-    },
-    replyCut: function(a){
-        return a.substring(0, 50)
-    }
-}
-=======
+
   ifCond: function(a, b, options) {
     if (a === b) {
       return options.fn(this)
@@ -29,4 +17,4 @@ module.exports = {
     return a.substring(0, 50)
   }
 }
->>>>>>> 1f13f35d5be2f94a62965805f06fd8449c68b404
+
