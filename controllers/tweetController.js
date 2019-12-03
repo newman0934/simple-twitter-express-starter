caf3c8ab8089438f2624caaf794f36c2f9ce3c7d
Eason Lin <uu9924079@gmail.com>
2019-12-03 15:33:26 +0800

    feat: login user see no follow button

--- controllers/tweetController.js
+++ controllers/tweetController.js
@@ -23,6 +23,11 @@ const tweetController = {
           }
         }
         sortedUserList = users.slice(0, 10)
+        // 如果 user 不是登入者加入 isNotCurrentUser 給 view 判定 follow 按鈕
+        sortedUserList.map(user => {
+          if (user.id !== helpers.getUser(req).id)
+            return (user.isNotCurrentUser = true)
+        })
       }
     )
     // 取得 /tweets 左半邊資料並渲染
