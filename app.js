898c019faa9d8f2542dfc0cb9c12aa252d65c875
java5575 <newman0934@gmail.com>
2019-12-03 22:36:39 +0800

    const port = process.env.PORT || 3000

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const bodyParser = require('body-parser')
 
 app.set('view engine', 'handlebars')
